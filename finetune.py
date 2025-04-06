53e7fb963c00155ceb57226c66062ef819e94d1f
Goustjager0 <chenjy2024@163.com>
2025-04-06 19:14:32 +0800

    Add files via upload

--- finetune.py
+++ finetune.py
@@ -178,54 +178,48 @@ def train(
     #     else:
     #         return result
     def preprocess_function(examples):
-        if len(examples["answers"]["text"]) == 0 or len(examples["answers"]["answer_start"]) == 0:
-            print("missing data")
-            return {}
-        # questions = [q.strip() for q in examples["question"]]
         inputs = tokenizer(
             examples["question"],
             examples["context"],
-            max_length=384,
             truncation="only_second",
+            max_length=384,
+            stride=128,
+            return_overflowing_tokens=True,
             return_offsets_mapping=True,
-            padding="max_length",
+            padding="max_length"
         )
 
-        offset_mapping = inputs.pop("offset_mapping")
-        answers = examples["answers"]
         start_positions = []
         end_positions = []
 
-        for i, offset in enumerate(offset_mapping):
-            answer = answers[i]
-            start_char = answer["answer_start"][0]
-            end_char = answer["answer_start"][0] + len(answer["text"][0])
-            sequence_ids = inputs.sequence_ids(i)
-
-            # Find the start and end of the context
-            idx = 0
-            while sequence_ids[idx] != 1:
-                idx += 1
-            context_start = idx
-            while sequence_ids[idx] == 1:
-                idx += 1
-            context_end = idx - 1
-
-            # If the answer is not fully inside the context, label it (0, 0)
-            if offset[context_start][0] > end_char or offset[context_end][1] < start_char:
+        for i in range(len(examples["answers"])):
+            if len(examples["answers"][i]["text"]) == 0 or len(examples["answers"][i]["answer_start"]) == 0:
+                # If no answer, use CLS token as placeholder
                 start_positions.append(0)
                 end_positions.append(0)
             else:
-                # Otherwise it's the start and end token positions
-                idx = context_start
-                while idx <= context_end and offset[idx][0] <= start_char:
-                    idx += 1
-                start_positions.append(idx - 1)
+                # Get start and end char positions
+                start_char = examples["answers"][i]["answer_start"][0]
+                end_char = start_char + len(examples["answers"][i]["text"][0])
 
-                idx = context_end
-                while idx >= context_start and offset[idx][1] >= end_char:
-                    idx -= 1
-                end_positions.append(idx + 1)
+                # Find the token span that matches
+                sequence_ids = inputs.sequence_ids(i)
+                offset_mapping = inputs["offset_mapping"][i]
+
+                context_start = sequence_ids.index(1)
+                context_end = len(sequence_ids) - 1 - sequence_ids[::-1].index(1)
+
+                token_start = token_end = 0
+                for idx in range(context_start, context_end):
+                    start, end = offset_mapping[idx]
+                    if start <= start_char < end:
+                        token_start = idx
+                    if start < end_char <= end:
+                        token_end = idx
+                        break
+
+                start_positions.append(token_start)
+                end_positions.append(token_end)
 
         inputs["start_positions"] = start_positions
         inputs["end_positions"] = end_positions
